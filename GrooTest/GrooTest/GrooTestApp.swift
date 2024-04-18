78fbd02536698e29e3aa066b0b06e350588996e4
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-18 20:30:24 +0900

    First commit

---
+++ GrooTest/GrooTest/GrooTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrooTestApp.swift
+//  GrooTest
+//
+//  Created by Groo on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GrooTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
